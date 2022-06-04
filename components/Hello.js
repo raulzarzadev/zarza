80ae0444cd7653e6b5cdd378547409792c0e3eb3
zasara <raulzarza@gmail.com>
2022-06-04 11:01:26 -0600

    add link raulzarza

--- components/Hello.js
+++ components/Hello.js
@@ -1,20 +1,25 @@
 import Image from "next/image"
+import Link from "next/link"
 import TypescriptEffect from "./TypescriptEffect"
 
 const Hello = () => {
-    return (
-      <div className='grid place-content-center'>
-        <div className='relative h-16 w-16 rotate-90'>
-          <Image src='/icon.png' layout='fill' objectFit='contain' alt="zarza-icon"/>
-        </div>
-        <h1 className='font-bold text-2xl'>
-          <TypescriptEffect label="Hi! I'm Zarza " />
-        </h1>
-        <p>
-          {`I'm web developer`}
-        </p>
+  return (
+    <div className='grid place-content-center'>
+      <div className='relative h-16 w-16 rotate-90'>
+        <Link href={'https://raulzarza.dev'}>
+          <a target='_blank'  rel="noreferrer">
+            <Image src='/icon.png' layout='fill' objectFit='contain' alt="zarza-icon" />
+          </a>
+        </Link>
       </div>
-    )
-  }
-  
+      <h1 className='font-bold text-2xl'>
+        <TypescriptEffect label="Hi! I'm Zarza " />
+      </h1>
+      <p>
+        {`I'm web developer`}
+      </p>
+    </div>
+  )
+}
+
 export default Hello
