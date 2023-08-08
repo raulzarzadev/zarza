45c5cccdbad4ccab3a3c43c2523681d64076f4e4
Raul Zarza <raulzarza.dev@gmail.com>
2023-08-08 14:08:54 -0700

    remove logs, lint some files

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -1,21 +1,22 @@
 //@ts-ignore
 /**
-* #You canot use image/next because de images are commign for multiple pages ...
-*
-*/
+ * #You can't use image/next because de images are coming for multiple pages ...
+ *
+ */
 const SkillCard = ({ skill = {} }) => {
-    const { name = 'skill name', image = '', experience = 'skill experinece' } = skill
-    return (
-        <div className='bg-base-300 h-32 w-44 flex '>
-            
-            <div>
-                <img src={image} alt={name} className='h-20' />
-            </div>
-            <h4>{name}</h4>
-            <p className='text-sm'>
-                {experience}
-            </p>
-        </div>
-    )
+  const {
+    name = 'skill name',
+    image = '',
+    experience = 'skill experience'
+  } = skill
+  return (
+    <div className="bg-base-300 h-32 w-44 flex ">
+      <div>
+        <img src={image} alt={name} className="h-20" />
+      </div>
+      <h4>{name}</h4>
+      <p className="text-sm">{experience}</p>
+    </div>
+  )
 }
-export default SkillCard  
+export default SkillCard
