981e1a61942609391b03485a1657a41249750f88
Raul Zarza <raulzarza.dev@gmail.com>
2023-08-09 14:19:27 -0700

    add sanity api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
+import Head from 'next/head'
+import Layout from 'comps/Layout'
+import BottomBar from 'comps/Navigation/BottomBar'
+import Hello from 'comps/Hello'
+import About from 'comps/About'
+import Works from 'comps/Works'
+import Jobs from 'comps/Jobs'
+import Contact from 'comps/Contact'
 
-
-import Head from "next/head";
-import Layout from "comps/Layout";
-import BottomBar from "comps/Navigation/BottomBar";
-import Hello from "comps/Hello";
-import About from "comps/About";
-import Works from "comps/Works";
-import Jobs from "comps/Jobs";
-import Contact from "comps/Contact";
-
-export default function Home() {
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    'https://dbftdq7i.api.sanity.io/v2023-06-21/data/query/production?query=*%5B_type+%3D%3D+%22work%22%5D'
+  )
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.log(err))
+  const works = res.result
+  return {
+    props: {
+      works
+    }
+  }
+}
+export default function Home(props: { works: any[] }) {
+  console.log(props?.works)
   const SECTIONS = {
     home: <Hello />,
     about: <About />,
@@ -22,16 +35,19 @@ export default function Home() {
   }
   const sections: string[] = Object.keys(SECTIONS)
 
-
   return (
     <>
       <Head>
         <title>Raúl Zarza</title>
       </Head>
-      <div className=' bg-violet-200  ' >
-        {sections.map(section => {
+      <div className=" bg-violet-200  ">
+        {sections.map((section) => {
           return (
-            <section key={section} id={section} className='min-h-screen flex justify-center items-center'>
+            <section
+              key={section}
+              id={section}
+              className="min-h-screen flex justify-center items-center"
+            >
               {SECTIONS[section as keyof typeof SECTIONS]}
             </section>
           )
