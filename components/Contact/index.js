f5cac640c44ea73529f002623e27eab68caf5a82
Raul Zarza <raulzarza.dev@gmail.com>
2024-01-12 13:36:51 -0700

    change contact me sectioon

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,33 +1,26 @@
 const Contact = () => {
-    return (
-        <div className="text-center">
-            <h3 className="">
-                {`  Whats's Nexts? `}
-            </h3>
-            <p className="text-4xl font-bold text-white">
-                {`Get In touch`}
-            </p>
-            <p className="whitespace-pre-line">
-                {`
-                I'm currently looking for new challenges 
-                and opportunities to co-create amazing products.
-                
-                If you have any questions,
-                 feel free to contact me.
-                `}
-            </p>
-            <div>
-                <a 
-                href="mailto:raulzarza.dev@gmail.com" 
-                className=" btn btn-outline my-10 " 
-                target='_blank' 
-                rel="noreferrer"
-                >
-                    {`Say Hello`}
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="text-center">
+      {/* <h3 className="">{`  Whats's Nexts? `}</h3> */}
+      <p className="text-4xl font-bold text-white">{`Get In touch`}</p>
+      <p className="whitespace-pre-line">
+        {`
+            I'm currently looking for new challenges 
+            and opportunities to co-create amazing products.
+         `}
+      </p>
+      <div>
+        <a
+          href="mailto:raulzarza.dev@gmail.com"
+          className=" btn btn-outline my-10 "
+          target="_blank"
+          rel="noreferrer"
+        >
+          {`Send me an email`}
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
